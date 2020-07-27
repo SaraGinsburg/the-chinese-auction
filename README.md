c288ae10e11262923198ea3dc3dad4f596134a2f
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-07-26 23:50:30 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Chinese Auction
+
+# step 1: create React App
+
+Project Development Workflow
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Chinese Auction</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All rights reserved.
+      </footer>
+    </div>
+  );
+}
+
+export default App;

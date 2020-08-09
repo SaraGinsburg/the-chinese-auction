93ce7095554ba92e072bc87a9039b74363a58e87
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-08-09 14:53:41 -0400

    create order

--- server.js
+++ server.js
@@ -65,6 +65,41 @@ app.delete("/api/products/:id", async (req, res) => {
   res.send(deletedProduct);
 });
 
+const Order = mongoose.model("order", new mongoose.Schema(
+    {
+        _id:{
+            type: String,
+            default: shortid.generate
+        },
+        email: String,
+        name: String,
+        address: String,
+        total: Number,
+        cartItems: [{
+            _id: String,
+            title: String,
+            price: Number,
+            count: Number
+        }]
+    },
+    {
+        timestamps: true,
+    }));
+
+app.post("/api/orders", async (req, res) => {
+    if(!req.body.name ||
+       !req.body.email ||
+       !req.body.address ||
+       !req.body.total ||
+       !req.body.cartItems 
+    ){
+        return res.send({message: "Data is required."})
+    }
+    const order = await Order(req.body).save();
+    res.send(order);
+
+})
+
 // listen to a port and launch the server
 // use process.env.Port, a special variable that sets the port number, or the default 5000
 const port = process.env.PORT || 5000;
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import formatCurrency from "../util";
 import Fade from "react-reveal/Fade";
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom"
 import { connect } from "react-redux";
 import { removeFromCart } from "../actions/cartActions";
+import { createOrder, clearOrder} from "../actions/orderActions";
 
 class Cart extends Component {
   constructor(props) {
@@ -24,19 +27,67 @@ class Cart extends Component {
       email: this.state.email,
       address: this.state.address,
       cartItems: this.props.cartItems,
+      total: this.props.cartItems.reduce((a,c) => a + c.price * c.count, 0),
     };
-    this.props.createOrder(order);
+    this.props.createOrder(order);  // by calling this method from props, the createOrder action will run.
   };
+  closeModal = () => {
+    this.props.clearOrder();
+  }
   render() {
-    const { cartItems } = this.props;
+    const { cartItems, order } = this.props;
     return (
       <div>
         {cartItems.length === 0 ? (
           <div className="cart cart-header">Cart is empty</div>
         ) : (
           <div className="cart cart-header">
-            You have {cartItems.length} in the cart{" "}
+            You have {cartItems.length} items in the cart{" "}
           </div>
+        )}
+        {
+          order && (
+          <Modal 
+            isOpen={true}
+            onRequestClose={this.closeModal} >
+            <Zoom>
+              <button className="close-modal" onClick={this.closeModal}>x</button>
+              <div className="order-details">
+                <h3 className="success-message">Your order has been placed.</h3>
+                <h2>Order {order._id}</h2>
+                <ul>
+                  <li>
+                    <div>Name:</div>
+                    <div>{order.name}</div>
+                  </li>
+                  <li>
+                    <div>Email:</div>
+                    <div>{order.email}</div>
+                  </li>
+                  <li>
+                    <div>Address:</div>
+                    <div>{order.address}</div>
+                  </li>
+                  <li>
+                    <div>Date:</div>
+                    <div>{order.createdAt}</div>
+                  </li>
+                  <li>
+                    <div>Total:</div>
+                    <div>{formatCurrency(order.total)}</div>
+                  </li>
+                  <li>
+                    <div>Cart Items:</div>
+                    <div>
+                    {order.cartItems.map((el)=>(
+                      <div>{el.count} {" x "} {el.title}</div>
+                    ))}
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </Zoom>
+          </Modal>
         )}
         <div>
           <div className="cart">
@@ -90,19 +141,19 @@ class Cart extends Component {
                     <form onSubmit={this.createOrder}>
                       <ul className="form-container">
                         <li>
-                          <label>Email</label>
+                          <label>Name</label>
                           <input
-                            name="email"
-                            type="email"
+                            name="name"
+                            type="text"
                             required
                             onChange={this.handleInput}
                           ></input>
                         </li>
                         <li>
-                          <label>Name</label>
+                          <label>Email</label>
                           <input
-                            name="name"
-                            type="text"
+                            name="email"
+                            type="email"
                             required
                             onChange={this.handleInput}
                           ></input>
@@ -134,4 +185,8 @@ class Cart extends Component {
   }
 }
 export default connect(
-  (state)=>({cartItems: state.cart.cartItems,}),{ removeFromCart })(Cart);
+  (state)=>({
+    order: state.order.order,
+    cartItems: state.cart.cartItems,
+  }),
+  { removeFromCart,createOrder,clearOrder})(Cart)
